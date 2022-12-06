783e9ce930c0772af6c6eae75fdfec5ff4b76a43
Anna <anek_ogonek@mail.ru>
2022-12-06 21:07:32 +0300

    Added zadacha 24

---
+++ seminar_4_functions/Zadacha_24/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 24:
+//Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А.
+
+int ResultSum (int digitA)
+{
+    int sum = 0;
+    for (int i = 1; i <= digitA; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+int DataEntryA (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}   
+
+//Программа:
+
+Console.Clear();
+
+int digitA = DataEntryA ("Введите число А: ");
+int result = ResultSum(digitA); // Цикл суммы цифр от 1 до А 
+Console.Write(digitA +"-> " + result);
