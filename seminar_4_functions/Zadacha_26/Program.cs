8e945e9fcd276f889f6025ef6e1d5df61efcbb65
Anna <anek_ogonek@mail.ru>
2022-12-07 21:10:00 +0300

    Added zadacha_26

---
+++ seminar_4_functions/Zadacha_26/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 26:
+//Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+int QuantityDigit (int number)
+{
+    int i = 1;
+    for (i = 1; number > 10; i++)
+    {
+        number = number/10;
+    }
+    return i;
+}
+
+
+int DataEntryNumber (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}   
+
+//Программа:
+
+Console.Clear();
+
+int number = DataEntryNumber("Введите число: ");
+int result = QuantityDigit(number); // Обращение к методу, считающему цифры
+Console.Write(number +"-> " + result);
