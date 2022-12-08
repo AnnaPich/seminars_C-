da72adb29beda213c5832dc3a57afe07072d58c1
Anna <anek_ogonek@mail.ru>
2022-12-08 17:16:29 +0300

    added zadacha_28

---
+++ seminar_4_functions/Zadacha_28/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int ResultProduct (int number)
+{
+    int product = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        product = product * i;
+    }
+    return product;
+}
+
+
+int DataEntryN (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}   
+
+//Программа:
+
+Console.Clear();
+
+int digitN = DataEntryN ("Введите число N: ");
+int result = ResultProduct(digitN); 
+Console.Write(digitN +"-> " + result);
