39a6c4716deec64a369cec6663defd2ecbc140dc
Anna <anek_ogonek@mail.ru>
2022-12-11 18:45:25 +0300

    added zadacha 32

---
+++ seminar_5_functions_and_array/Zadacha_32/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 32: 
+//Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] arr = { -4, -8, 8 ,2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
