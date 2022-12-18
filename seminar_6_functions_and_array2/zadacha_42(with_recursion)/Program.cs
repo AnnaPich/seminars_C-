a1f9a8bfc456bebc0f13de7e82f11d6315544061
Anna <anek_ogonek@mail.ru>
2022-12-18 19:08:18 +0300

    added zadacha 42, solution using recurion

---
+++ seminar_6_functions_and_array2/zadacha_42(with_recursion)/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 42: 
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+Console.Clear();
+
+void GetBinareView (int numb)
+{
+    if(numb == 0) return;
+    GetBinareView(numb/2);
+    System.Console.Write(numb % 2); //вывод с конца (с последнего значения), обратить внимание
+}
+
+
+int DataEntryNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+//Программа:
+
+int number = DataEntryNumber("введите число: ");
+GetBinareView (number);
