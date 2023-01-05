452ac3be6ea5493ce30c5904744bc4f86f8cfad3
Anna <anek_ogonek@mail.ru>
2023-01-05 20:53:29 +0400

    added zadacha 63

---
+++ seminar_9_recursion/zadacha_63/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 63:
+//Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5" 
+
+
+//Print numbers:
+void PrintNumbers(int number)
+{
+    if (number == 0) return;
+    PrintNumbers(number - 1);
+    Console.Write(number + ", ");
+}
+
+//Receiving a namber from the user:
+int DataEntyNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//Program:
+int number = DataEntyNumber("Введите число N:");
+PrintNumbers(number);
