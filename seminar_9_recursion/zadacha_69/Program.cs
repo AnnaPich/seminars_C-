9d1ad87f1e9d1f55366f87704867b83c3e0a92c6
Anna <anek_ogonek@mail.ru>
2023-01-06 22:50:26 +0400

    added zadacha 69

---
+++ seminar_9_recursion/zadacha_69/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 69:
+//Напишите программу, которая на вход принимает
+//два числа А и В, и возводит число А в целую степень В с помощью рекурсии.
+
+
+//Exponentiation number A in numer B:
+void NumberAExpNamberB(int numberFirst, int numberSecond, int count, int digit)
+{
+    if(count == numberSecond)
+    {
+        Console.WriteLine($"Число {numberFirst} в степени {numberSecond} равно " + digit);
+        return;
+    }
+    digit *= numberFirst;
+    count ++;
+    NumberAExpNamberB(numberFirst, numberSecond, count, digit);
+}
+
+//Receiving a namber from the user:
+int DataEntyNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//Program:
+int numberFirst = DataEntyNumber("Введите число А: ");
+int numberSecond = DataEntyNumber("Введите число B: ");
+
+int count = 1;
+int digit = numberFirst;
+NumberAExpNamberB(numberFirst, numberSecond, count, digit);
